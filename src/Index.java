ec97e755e58500df760af638178ec81dc8ad36cb
Alexey Romanenko <githubalexrom@gmail.com>
2020-02-15 15:13:34 -0500

    Create Index.java

---
+++ src/Index.java
@@ -0,0 +1,21 @@
+/** This class suppose to be the main class to launch the program.
+ * 
+ * Please read the Readme.md file to get familiar with the program purposes
+ * 
+ */
+
+/**
+ * @author 70691
+ *
+ */
+public class Index {
+	public static void main (String[] args) {
+		
+		
+		// STEP 1 : Check that all systems is ready
+		
+		System.out.println("Is the phone mirrore with this comuter? : Y or NO");
+		
+	}
+
+}
