deadc7bd216c1706bef04d80159f37a8d38cd5e2
Alexey Romanenko <githubalexrom@gmail.com>
2020-02-21 19:06:23 -0500

    Update Index.java
    figured out the obstacles with TAP-game
    created main idea of SWAP game, meet obstacle figured it out  - will resolve later

--- src/Index.java
+++ src/Index.java
@@ -48,7 +48,9 @@ public class Index {
 // STEP 3 : Make a mouse-click to initiate a miniGame call
 		
 		// Move mouse to the PinPoint and click
-		mouseClick(pinPoint);
+		// shift on X to get out from PinPoint
+		int shift = 10;
+		mouseClick(pinPoint, shift);
 		System.out.print("\nMining initiated");
 		sleep();
 		
@@ -86,25 +88,107 @@ public class Index {
 	}
 
 	// A Method to play SWIPE miniGame
-	private static void playSwipe(int[] pinPoint) {
+	private static void playSwipe(int[] pinPoint) throws AWTException, InterruptedException {
+		sleep();
 		System.out.print("\nPlaying Swipe");
+		//steps to coordinates of CheckPoint from PinPoint (claim white circle)
+				int stepPPCHPX = 50;
+				int stepPPCHPY = 80;
+		// Steps from TOP CheckPoint to the PinPoint
+				int stepTopPPX = 90;
+				int stepTopPPY = 190;
+		// Steps from RIGHT CheckPoint to the PinPoint
+				int stepRightPPX = 130;
+				int stepRightPPY = 70;
+		// Steps from Bottom CheckPoint to the PinPoint
+				int stepBottomPPX = 66;
+				int stepBottomPPY = 170;
+		// Steps from LEFT CheckPoint to the PinPoint
+				int stepLeftPPX = 132;
+				int stepLeftPPY = 50;
 		
-	}
+		// Main Check point coordinates
+				int[] mainCHP = new int[] {pinPoint[0] - stepPPCHPX, pinPoint[1] - stepPPCHPY};
+		// Top Check point coordinates
+				int[] topCHP = new int[] {pinPoint[0] - stepTopPPX, pinPoint[1] - stepTopPPY};
+		// Right Check point coordinates
+				int[] rightCHP = new int [] {pinPoint[0] + stepRightPPX, pinPoint[1] - stepRightPPY};
+		// Bottom Check point coordinates
+				int[] bottomCHP = new int[] {pinPoint[0] - stepBottomPPX, pinPoint[1] + stepBottomPPY};
+		// Left Check point coordinates
+				int[] leftCHP = new int[] {pinPoint[0] - stepLeftPPX, pinPoint[1] + stepLeftPPY};
+				
+				Robot bot = new Robot();
+				
+		// Color at Main Check Point
+				int mainColor = bot.getPixelColor(mainCHP[0], mainCHP[1]).getRGB();
+		// Top color
+				int topColor = bot.getPixelColor(topCHP[0], topCHP[1]).getRGB();
+		// Right color
+				int rightColor = bot.getPixelColor(rightCHP[0], rightCHP[1]).getRGB();
+		// Bottom Color
+				int bottomColor = bot.getPixelColor(bottomCHP[0], bottomCHP[1]).getRGB();
+		// Left Color
+				int leftColor = bot.getPixelColor(leftCHP[0], leftCHP[1]).getRGB();
+				
+				// Checking loop
+				int loop = 0;
+				
+				while (loop<5) {
+					if (mainColor == topColor) {
+						mouseClick(topCHP, 0);
+					}
+					else if(mainColor == rightColor){
+						mouseClick(rightCHP, 0);
+					}
+					else if(mainColor == bottomColor) {
+						mouseClick(bottomCHP, 0);
+					}
+					else {
+						mouseClick(leftCHP, 0);
+						}
+					System.out.print(loop++);
+					Thread.sleep(500);
+					//if (bot.getPixelColor(pinPoint[0], pinPoint[1]).getRGB() == pinPoint[2]) {
+						//break;
+				//	}
+					}
+				}
+		
 	
 	// A Method to play TAP miniGame
 	private static void playTap(int[] pinPoint) throws AWTException, InterruptedException {
+		
+		//steps to coordinates of CheckPoint from PinPoint (claim white circle)
+		int stepPPCHPX = 90;
+		int stepPPCHPY = 173;
+		
+		// Steps from PinPoint to the center of first tile
+		int stepPPTLX = 110;
+		int stepPPTLY = 110;
+		
+		// Step between tiles 
+		int stepTile = 100;
+		
 		System.out.print("\nPlaying Tap - ");
 		Robot bot = new Robot();
-		int count = 0;		
-		//while (bot.getPixelColor(pinPoint[0]+62, pinPoint[1]+97).getRGB() == -13031862 ) {
-		while (count <5) {
-		for( int x = pinPoint[0]-63; x<= 270; x += 60) {
-			for( int y = pinPoint[1]-67; y <= 380; y += 60) {
+		int count = 0;	
+		
+		while (bot.getPixelColor(pinPoint[0]+stepPPCHPX, pinPoint[1]+stepPPCHPY).getRGB() == -10592674) {
+		//while (count <5 ) {
+		loop:	
+		for( int x = pinPoint[0]-stepPPTLX; x < 350; x += stepTile) {
+			for( int y = pinPoint[1]-stepPPTLY; y < 590; y += stepTile) {
+				
 				if (bot.getPixelColor(x, y).getRGB() != -11315627) {
-					mouseClick(new int[]{x,y});
+					mouseClick(new int[]{x,y},0);
 					count++;
 					System.out.print(count);
 					}
+				Thread.sleep(100);
+				if (bot.getPixelColor(pinPoint[0]+stepPPCHPX, pinPoint[1]+stepPPCHPY).getRGB() != -10592674) {
+					break loop;
+				}
 				}
 			}
 		}
@@ -113,7 +197,13 @@ public class Index {
 	
 	
 	// A Method to identify the miniGame and get its name
-	private static String getGameName(int[] pinPoint) throws AWTException {
+	private static String getGameName(int[] pinPoint) throws AWTException, InterruptedException {
+		Thread.sleep(500);
+		
+		// Steps from PinPoint coordinates to the CheckPoint coordinates (letter "S" in the
+		// name of the Swipe game
+		int stepX = 90;
+		int stepY = 173;
 			
 		// Empty string to store the name of miniGame		
 		String name = "";
@@ -121,7 +211,7 @@ public class Index {
 		// robot to get colors at specified points
 		Robot bot = new Robot();
 
-		if (bot.getPixelColor(pinPoint[0]+62, pinPoint[1]+97).getRGB() != -13031862) {
+		if (bot.getPixelColor(pinPoint[0]+stepX, pinPoint[1]+stepY).getRGB() != -10592674) {
 			name = "Swipe";
 		}
 		else {
@@ -137,12 +227,12 @@ public class Index {
 		
 	}
 	// A Method to move mouse to the coordinates and make a click
-	private static void mouseClick(int[] coordinates) throws AWTException, InterruptedException {
+	private static void mouseClick(int[] coordinates, int shift) throws AWTException, InterruptedException {
 		// Create a robot
 		Robot player = new Robot();
 		
 		// Move mouse to the PinPoint and click
-		player.mouseMove(coordinates[0], coordinates[1]);	
+		player.mouseMove(coordinates[0]+shift, coordinates[1]);	
 		player.delay(500);
 		
 		player.mousePress(InputEvent.BUTTON1_DOWN_MASK);
@@ -155,6 +245,8 @@ public class Index {
 	// This method made out from findImageCoordinates Method from class Find_Image_Get_Coordinates
 	private static int[] getPinPointCoordinates() throws AWTException, IOException {
 		
+		// Step from HomeButton Y to the PinPoint Y
+		int step = 332;
 		// The path to the image file which serves as a anchor-point 
 		//to calculate all coordinates in the mirrored screen
 		String imageName = "src/img/HomeButton.png";
@@ -192,7 +284,7 @@ public class Index {
 				if (pixelsMathed) {				
 					
 					coordinates[0] = x;// - icon.getWidth();
-					coordinates[1] = y-194;// - icon.getHeight();
+					coordinates[1] = y-step;// - icon.getHeight();
 					coordinates[2] = robot.getPixelColor(coordinates[0], coordinates[1]).getRGB();
 					break loopscreen;
 					}
