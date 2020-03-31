c99b3d13b5522ec6c347b44c5bc99dca25e959db
Alexey Romanenko <githubalexrom@gmail.com>
2020-03-31 18:29:03 -0400

    Update Drop.java

--- V_02/src/main/Drop.java
+++ V_02/src/main/Drop.java
@@ -5,50 +5,41 @@ import java.io.IOException;
 
 public class Drop {
 	
-	public static void main(String[] args) throws AWTException, IOException, InterruptedException {
-		
+	
+	public static void run() throws InterruptedException, AWTException, IOException {
 		// Amount to drop
-		int amount = 1000;
+				int amount = 2000;
+				
+				// Instance of Starter class to get pinPoin coordinates
+				Starter bot = new Starter();
+				
+				// shifts from Pinpoint to the drop-button
+				int dropX = -118;
+				int dropY = 183;
+				// Array of coordinates to drop button
+				int[] drop_button = new int []{bot.getPinPoint()[0]+dropX, bot.getPinPoint()[1]+dropY};
+				
+				// shifts from pinPoint to the amount scroll
+				int scrollX = 101;
+				int scrollY = -95;
+				// Array of coordinates to the amount scroll
+				int[] scroll = new int []{bot.getPinPoint()[0]+scrollX, bot.getPinPoint()[1]+scrollY};
+				
+				// shifts from PinPoint to the "current" button
+				int currentX = -18;
+				int currentY = -52;
+				// Array of coordinates to "current" button
+				int[] current = new int []{bot.getPinPoint()[0]+currentX, bot.getPinPoint()[1]+currentY};
+				
+				// shifts from Pinpoint to the "ok" button
+				int okX = 66;
+				int okY = 173;
+				// Array of coordinates to the "ok" button
+				int[] ok = new int []{bot.getPinPoint()[0]+okX, bot.getPinPoint()[1]+okY};
 		
-		// Instance of Starter class to get pinPoin coordinates
-		Starter bot = new Starter();
-		
-		// shifts from Pinpoint to the drop-button
-		int dropX = -118;
-		int dropY = 183;
-		// Array of coordinates to drop button
-		int[] drop_button = new int []{bot.getPinPoint()[0]+dropX, bot.getPinPoint()[1]+dropY};
-		
-		// shifts from pinPoint to the amount scroll
-		int scrollX = 101;
-		int scrollY = -95;
-		// Array of coordinates to the amount scroll
-		int[] scroll = new int []{bot.getPinPoint()[0]+scrollX, bot.getPinPoint()[1]+scrollY};
-		
-		// shifts from PinPoint to the "current" button
-		int currentX = -18;
-		int currentY = -52;
-		// Array of coordinates to "current" button
-		int[] current = new int []{bot.getPinPoint()[0]+currentX, bot.getPinPoint()[1]+currentY};
-		
-		// shifts from Pinpoint to the "ok" button
-		int okX = 66;
-		int okY = 173;
-		// Array of coordinates to the "ok" button
-		int[] ok = new int []{bot.getPinPoint()[0]+okX, bot.getPinPoint()[1]+okY};
-		
-		// counter
 		int counter = 0;
-		
-		while(counter<=amount) {
-		// A Method to run drops
-			run(drop_button, scroll, current, ok);
-			counter = counter+200;	
-		}
-	}
-
-	private static void run(int[] drop_button, int[] scroll, int[] current, int[] ok) throws InterruptedException, AWTException {
-		
+		while(counter<amount) {
+			
 		Mouse.click(drop_button);
 		 Thread.sleep(1500);
 		 
@@ -60,6 +51,10 @@ public class Drop {
 		 
 		 Mouse.click(ok);
 		 Thread.sleep(1500);
+		 counter = counter+200;
+		 System.out.println ("Total droped: " + counter);
+		 
+		}
 	}
 
 }
