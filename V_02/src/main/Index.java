e39832d7593d0b4daddbaa8517507f736cc64441
Alexey Romanenko <githubalexrom@gmail.com>
2020-03-31 18:29:18 -0400

    Update Index.java

--- V_02/src/main/Index.java
+++ V_02/src/main/Index.java
@@ -11,7 +11,7 @@ public class Index {
 	public static void main(String[] args) throws AWTException, IOException, InterruptedException {
 
 		// Amount of games to be played
-		int playGames = 2000;
+		int playGames = 1000000;
 		// Games played counter
 		int countGames = 0;
 
@@ -27,11 +27,28 @@ public class Index {
 
 			Thread.sleep(400);
 
+			//Identify and play the game
 			Game.play(bot.getPinPoint());
 			
-			BigGeoMine.check(bot.getPinPoint());
+			// Check pop-up
+			PopUpCheck.check(bot.getPinPoint());
 
 			Thread.sleep(1400);	
+			
+			// Pause condition
+			if(countGames%700==0) {
+				
+				System.out.println("\n_______PAUSE_______");
+				System.out.println("______________________");
+				System.out.println(LocalDateTime.now());
+				
+				// Drop
+			//	Drop.run();
+				
+				//Thread.sleep((int)(Math.random()*60000)+300000);
+			}
+			
+			
 		}
 		System.out.println("\n_______FINISH_______");
 		System.out.println("______________________");
